f5eccf4a4ab4ba8201e84fb2c09d5dc090f7f4a3
rex <0xt_r3x@keemail.me>
2025-06-15 16:17:08 +0530

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -1,52 +1,49 @@
-var youtube   = "https://www.youtube.com/@0x7_r3x/";
-var x         = "https://www.x.com/0x7_r3x/";
-var linkedin  = "https://www.linkedin.com/in/0x7_r3x/";
+var youtube = "https://www.youtube.com/@0x7_r3x/";
+var x = "https://www.x.com/0x7_r3x/";
+var password = "a810b5df8e939419ad5a730383a4f6b7b150d3635a37c1b2ca08f07b1102bbc5";
+var linkedin = "https://www.linkedin.com/in/0x7_r3x/";
 var instagram = "https://www.instagram.com/0x7_r3x/";
-var github    = "https://github.com/0x7-r3x/";
-var h1        = "https://hackerone.com/0x7_r3x/";
-var games     = "https://minecraft.net/";
-var email     = "mailto:0xt_r3x@intigriti.me";
-var password  = "a810b5df8e939419ad5a730383a4f6b7b150d3635a37c1b2ca08f07b1102bbc5";
+var github = "https://github.com/0x7-r3x/";
+var games = "https://minecraft.net/";
+var h1 = "https://hackerone.com/aihxdev/";
+var email = 'mailto:0xt_r3x@intigriti.me';
 
-// commands
-
-var whois = [
+whois = [
   "<br>",
-  "Bounty Hunter",
+  "Hey, I'm B0unty Hunter!",
   "<br>"
 ];
 
-var whoami = [
+whoami = [
   "<br>",
-  "hey, myself I'm Bounty Hunter, love to crack computers",
+  "--",
   "<br>"
 ];
 
-var social = [
+social = [
   "<br>",
-  'youtube        <a href="' + youtube + '" target="_blank">youtube/@0x7_r3x</a>',
-  'x              <a href="' + x + '" target="_blank">x/@0x7_r3x</a>',
-  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/0x7_r3x</a>',
-  'instagram      <a href="' + instagram + '" target="_blank">instagram/0x7_r3x</a>',
-  'github         <a href="' + github + '" target="_blank">github/0x7-r3x</a>',
-  'hackerone      <a href="' + h1 + '" target="_blank">hackerone/0x7_r3x</a>',
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/@0x7_r3x' + "</a>",
+  'x              <a href="' + x + '" target="_blank">x/@0x7_r3x' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/0x7_r3x' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/0x7_r3x' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/0x7-r3x' + "</a>",
+  'hackerone      <a href="' + h1 + '" target="_blank">hackerone/0x7_r3x' + "</a>",
   "<br>"
 ];
 
-var secret = [
+secret = [
   "<br>",
   '<span class="command">sudo</span>           Only use if you\'re admin',
   "<br>"
 ];
 
-var projects = [
+projects = [
   "<br>",
-  'June 15, 2025 built this website',
-  'games         <a href="' + games + '" target="_blank">minecraft.net</a>',
+  'games         <a href="' + games + '" target="_blank">https://github.com/aihxdev/flashmatrix' + "</a>",
   "<br>"
 ];
 
-var help = [
+help = [
   "<br>",
   '<span class="command">whois</span>          Who is rex?',
   '<span class="command">whoami</span>         Who are you?',
@@ -59,14 +56,21 @@ var help = [
   '<span class="command">email</span>          Do not email me',
   '<span class="command">clear</span>          Clear terminal',
   '<span class="command">banner</span>         Display the header',
-  "<br>"
+  "<br>",
 ];
 
-var banner = [
-  '<span class="index">rex (0x7_r3x). no rights reserved, ©2025.</span>',
-  "open source;",
-  " ________________________________________",
-  "/ Machines never cheat! \\",
-  '<span class="color2">hax terminal a stolen one.</span>',
-  '<span class="color2">you know wt todo, those who don\'t will scream for</span> <span class="command">help</span><span class="color2">.</span>',
-];
+  banner = [
+        '<span class="index">rex (0x7_r3x). no rights reserved.</span>',
+        "open source;",
+        " ________________________________________",
+        "/ Machines never cheat!                          \\",
+        '<span class="color2">hax terminal a stolen one.</span>',
+    "<span class=\"color2\">you know wt todo, those who don't will scream for -</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+    ];
+
+    projects = [
+      "<br>",
+      'June 15, 2025 created this website',
+      "<br>"
+    ];
+    
