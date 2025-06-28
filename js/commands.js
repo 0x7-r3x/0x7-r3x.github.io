ffac8025ed2c46a1bca5479582b9f1de62f2f0aa
rex <0xt_r3x@keemail.me>
2025-06-28 21:12:17 +0530

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -10,7 +10,7 @@ var email = 'mailto:0xt_r3x@intigriti.me';
 
 whois = [
   "<br>",
-  "Yo, I'm surfer of treches in the internet for hunting bugs, building things, chasing systems, Ever since I was a child I been hunter , hunting for things I want to kill them for me to level-up and seek growth, well hacking is never ending & the deapth, I was meant to live in dark trenches of the world of Internet.",
+  "Yo, I'm surfer of treches in the internet for hunting bugs, building things, chasing systems, Ever since I was a child I been hunter , \n hunting for things I want to kill them for me to level-up and seek growth, well hacking is never ending & the deapth, I was meant to live in dark trenches of the world of Internet.",
   "<br>"
 ];
 
